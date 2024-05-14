2f32885db464c36349f36eb07d1aca2a007b9d18
TahaThinks <tahaa4587@gmail.com>
2024-05-14 10:38:27 +0400

    Create API Header

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ import requests
 APP_ID = api_data.NUTRITION_APPID
 APP_KEY = api_data.NUTRITION_APPKEY
 
+headers = {
+    "x-app-id": APP_ID,
+    "x-app-key": APP_KEY,
+    "x-remote-user-id": "0"
+}
+
