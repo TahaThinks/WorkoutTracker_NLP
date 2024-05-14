07bb59f257f2d9c7b507817e4dcc95081621f796
TahaThinks <tahaa4587@gmail.com>
2024-05-14 11:05:31 +0400

    Filter Data from response

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import requests
 import datetime
 
 current_day = datetime.datetime.now().strftime("%d/%m/%Y")
+current_time = datetime.datetime.now().strftime("%H:%M:%S")
 
 APP_ID = api_data.NUTRITION_APPID
 APP_KEY = api_data.NUTRITION_APPKEY
+SHEET_ENDPOINT = api_data.SHEET_ENDPOINT
 
 headers = {
     "x-app-id": APP_ID,
@@ -17,8 +19,18 @@ NLP_URL = "https://trackapi.nutritionix.com"
 NLP_ENDPOINT = "/v2/natural/exercise"
 
 workout_text = {
-    "query": "I swam for 1 hour"
+    "query": "I swam for 1 hour and went cycling for 30 minutes"
 }
 
 response = requests.post(url=f"{NLP_URL}{NLP_ENDPOINT}", headers=headers, json=workout_text)
 print(response.text)
+
+for exercise in response.json()["exercises"]:
+    exercise_data = {
+        "Date": current_day,
+        "Time": current_time,
+        "Exercise": exercise["name"],
+        "Duration": exercise["duration_min"],
+        "Calories": exercise["nf_calories"]
+    }
+    print(exercise_data)
